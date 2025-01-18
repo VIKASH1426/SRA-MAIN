7b49c4686a193301431fb2fb6976f977fa303dcf
VIKASH1426 <103167572+VIKASH1426@users.noreply.github.com>
2025-01-19 00:46:57 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import nltk
 import spacy
-nltk.download('stopwords')
-spacy.load('en_core_web_sm')
+
+# Ensure the spaCy model is installed and loaded
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 import pandas as pd
 import base64, random
